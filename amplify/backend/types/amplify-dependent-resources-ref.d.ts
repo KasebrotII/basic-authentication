57c6ce89c1f82dce69a4cd31e62774efc8612017
Sheldon <kasebrot@live.com>
2023-11-08 15:16:28 -0600

    Ran amplify push...
    also ran npx update-browserslist-db@latest because caniuse-lite is outdated.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "basicauthentication280ac816": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
