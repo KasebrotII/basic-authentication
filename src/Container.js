5f7bfbd7cc3daa55b817f5f7cb7caa1fbdd8be5b
Sheldon <kasebrot@live.com>
2023-11-12 16:42:55 -0600

    Created components for Container and Public files.

---
+++ src/Container.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+export const Container = ({ children }) => (
+  <div style={styles.container}>
+    { children }
+  </div>
+);
+
+const styles = {
+  container: {
+    margin: '0 auto',
+    padding: '50px 100px'
+  }
+};
---
+++ src/Public.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from './Container';
+
+
+
+export const Public = () => {
+  return (
+    <Container>
+      <h1>Public route</h1>
+    </Container>
+  )
+};
